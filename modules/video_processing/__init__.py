5472380f0311584c6f3ee5d26ab01e778583bddf
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-06-08 23:39:10 -0300

    :wrench: feat: [WIP] Improve `VideoProcessor` command
    Draw contour in the biggest element

--- modules/video_processing/__init__.py
+++ modules/video_processing/__init__.py
@@ -1,9 +1,17 @@
 import click
+import cv2
 from modules.color_segmentation import parseColorRange
 from modules.color_segmentation.identify_color import IdentifyColor
 from modules.element_contours.biggest_calculator import BiggestElementCalculator
-from modules.element_contours.element_extractor import ElementContourExtractor
 from modules.video_processing.video_processor import VideoProcessor
+from utils.cv_helpers import draw_contours
+
+
+def frame_contour_processor(
+    algorithm: BiggestElementCalculator, image_obj: cv2.Mat
+) -> cv2.Mat:
+    contours, frame = algorithm.execute(image=image_obj, is_sorted=True)
+    return draw_contours(frame, contours)
 
 
 @click.command()
@@ -18,21 +26,21 @@ from modules.video_processing.video_processor import VideoProcessor
 @click.argument("filename")
 def video_processor(color_range: str, output: str, filename: str):
     """
-    Open a video and extract the biggest element in it sorted by color
+    Open a video and generate a new one, showing the biggest element in it sorted by color
     """
 
     target_color = parseColorRange(color_range, False)
     color_threshold = IdentifyColor()
-    algorithm = BiggestElementCalculator(
-        threshold_func=lambda image: color_threshold.segmentate(
-            image=image, color_range=target_color
-        ),
-        isModeTree=False,
-    )
 
     extractor = VideoProcessor(
-        frame_processor=lambda image_obj: (
-            algorithm.execute(image=image_obj, is_sorted=True)
-        )[1]
+        frame_processor=lambda image_obj: frame_contour_processor(
+            image_obj=image_obj,
+            algorithm=BiggestElementCalculator(
+                threshold_func=lambda image: color_threshold.segmentate(
+                    image=image, color_range=target_color
+                ),
+                isModeTree=False,
+            ),
+        )
     )
     extractor.execute(filename, output_folder=output, debug_mode=True)
